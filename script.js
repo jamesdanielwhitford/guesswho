602d345c595f1126c1ee0e58bf25fcdcbd0d12de
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-01-06 14:37:23 +0200

    Delete server.js

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
     const gameBoard = document.getElementById('game-board');
     const flipSound = document.getElementById('flip-sound');
+    flipSound.preload = 'auto';
+    flipSound.load();
+    const imagePaths = [];
 
-    // Fetch the images from the server
-    fetch('/images')
-        .then(response => response.json())
-        .then(imagePaths => {
-            imagePaths.forEach((path, index) => {
-                const imageCard = document.createElement('div');
-                imageCard.classList.add('image-card');
+    // Generate image paths
+    for (let i = 1; i <= 24; i++) {
+        imagePaths.push(`images/image${i}.jpeg`); // Adjust the file extension if needed
+    }
+
+    imagePaths.forEach((path, index) => {
+        const imageCard = document.createElement('div');
+        imageCard.classList.add('image-card');
+        imageCard.style.backgroundImage = `url('${path}')`; // Set initial background image
+        imageCard.style.backgroundSize = 'cover'; // Ensure image covers the card
+        imageCard.addEventListener('click', () => {
+            flipSound.currentTime = 0;
+            flipSound.play();
+            if (imageCard.classList.contains('flipped')) {
                 imageCard.style.backgroundImage = `url('${path}')`;
-
-                imageCard.addEventListener('click', () => {
-                    flipSound.currentTime = 0;
-                    flipSound.play(); // Play the sound
-
-                    if (imageCard.classList.contains('flipped')) {
-                        imageCard.style.backgroundImage = `url('${path}')`;
-                        imageCard.classList.remove('flipped');
-                    } else {
-                        imageCard.style.backgroundImage = '';
-                        imageCard.classList.add('flipped');
-                    }
-                });
-
-                gameBoard.appendChild(imageCard);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching images:', error);
+                imageCard.classList.remove('flipped');
+            } else {
+                imageCard.style.backgroundImage = '';
+                imageCard.classList.add('flipped');
+            }
         });
+        gameBoard.appendChild(imageCard);
+    });
 });
